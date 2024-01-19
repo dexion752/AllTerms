cd375159bee15d82b5d97a62f1908bc1cfb0f94a
dexion <dexion.kong@gmail.com>
2024-01-19 14:23:42 +0900

    package modified: python-decouple, pagging modified.

--- qna/views.py
+++ qna/views.py
@@ -4,10 +4,20 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 def index(request):
+    MAX_LIST_CNT = 15
+    last_page_num = 0
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = { 'question_list' : question_list }
+    paginator = Paginator(question_list, MAX_LIST_CNT)
+    for page_num in paginator.page_range:
+        last_page_num = last_page_num + 1
+    page_obj = paginator.get_page(page)
+    context = {'question_list' : page_obj,
+               'last_page_num' : last_page_num,
+               }
     return render(request, 'qna/question_list.html', context)
 
 
