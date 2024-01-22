62bfd68d969859f351b2f6faf8a4825c7861411f
dexion <dexion.kong@gmail.com>
2024-01-22 17:15:09 +0900

    qna app and models(Question, Answer) created.

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qna.apps.QnaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
