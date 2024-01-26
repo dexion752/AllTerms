0b7f7baccb04e909448dc5aad2dcd945411784b9
dexion <dexion.kong@gmail.com>
2024-01-26 15:58:57 +0900

    tampletetag etc created. signin, login, logout function created.

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from qna import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('qna/', include('qna.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index')
 ]
