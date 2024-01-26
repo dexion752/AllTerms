a6043573f65edcdf8aece5eb62478e036492b847
dexion <dexion.kong@gmail.com>
2024-01-26 16:52:49 +0900

    qna/models modified

--- qna/views.py
+++ qna/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     MAX_LIST_CNT = 15
@@ -27,27 +26,31 @@ def detail(request, question_id):
     return render(request, 'qna/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('qna:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question' : question, 'form' : form}
     return render(request, 'qna/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('qna:index')
