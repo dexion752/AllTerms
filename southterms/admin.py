17342a2764c19bdd4473f4cbe62e85ea78c04cc8
dexion <dexion.kong@gmail.com>
2024-08-21 15:56:19 +0900

    admin 페이지 수정

--- southterms/admin.py
+++ southterms/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+@admin.register(Sources)
+class SourcesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'code', 'sum')
+    search_fields = ('title', 'author', 'code')
+    ordering = ['id', 'title']
+
+
+# admin.site.register(Sources, SourcesAdmin)
+
+# Register your models here.
